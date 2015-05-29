77ffd60c0de9131c0a945f43cd59f3f4e35f6a82
luis.gomez <luis.gg1990@gmail.com>
2015-05-28 22:26:13 -0300

    Restored VS 2012 compatibility

--- src/ReallyStopDebugger/Properties/AssemblyInfo.cs
+++ src/ReallyStopDebugger/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("lggomez")]
 [assembly: AssemblyProduct("ReallyStopDebugger")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("lggomez 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]   
 [assembly: ComVisible(false)]     
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 
