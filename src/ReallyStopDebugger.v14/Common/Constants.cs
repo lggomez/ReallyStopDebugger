50c7973f22df4b7aaf29b6a133b285607f98e18a
Luis G <luis.gg1990@gmail.com>
2015-08-14 00:33:49 -0300

    VS 2015 solution refactor (decoupling)
    File references fixed

--- src/ReallyStopDebugger.v14/Common/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace lggomez.ReallyStopDebugger.Common
-{
-    public static class Constants
-    {
-        public const int PROCESSESKILLERROR = -1;
-        public const int PROCESSESKILLSUCCESS = 0;
-        public const int PROCESSESNOTFOUND = 1;
-    }
-}
