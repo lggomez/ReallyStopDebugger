232004d80902c4c3583e6765c1ca2a13aa2a7980
Luis G <luis.gg1990@gmail.com>
2016-12-07 00:47:11 -0300

    Missing header

--- src/ReallyStopDebugger.v14/Native/SafeHandles/SafeGDIObjectHandle.cs
+++ src/ReallyStopDebugger.v14/Native/SafeHandles/SafeGDIObjectHandle.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Luis Gómez. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the src\ReallyStopDebugger directory for full license information.
+
+using System;
 using System.Runtime.InteropServices;
 
 namespace ReallyStopDebugger.Native.SafeHandles
